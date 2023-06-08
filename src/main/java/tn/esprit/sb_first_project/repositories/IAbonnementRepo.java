7a3b599c5309d4e0c346a1aed8588babaeb0eab0
Yosri BEN SAAD <yosri.bensaad@esprit.tn>
2023-06-08 07:00:14 +0100

    Ajout des queries

--- src/main/java/tn/esprit/sb_first_project/repositories/IAbonnementRepo.java
+++ src/main/java/tn/esprit/sb_first_project/repositories/IAbonnementRepo.java
@@ -1,8 +1,24 @@
 package tn.esprit.sb_first_project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tn.esprit.sb_first_project.entities.Abonnement;
+import tn.esprit.sb_first_project.entities.TypeAbonnement;
+
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface IAbonnementRepo extends JpaRepository<Abonnement,Long> {
+
+
+    @Query("select a from Abonnement a where a.typeAbon =: typeA order by a.dateDebut")
+    List<Abonnement> getAbonnementsOrderByDateDebut(@Param("typeA") TypeAbonnement typeAbon);
+
+    @Query("select a from Abonnement a where a.dateDebut between :date1 and :date2")
+    List<Abonnement> getAbonnementsBetweenDates(@Param("date1") Date date1, @Param("date2") Date date2);
+
+
 }
--- src/main/java/tn/esprit/sb_first_project/repositories/IInscriptionRepo.java
+++ src/main/java/tn/esprit/sb_first_project/repositories/IInscriptionRepo.java
@@ -1,8 +1,24 @@
 package tn.esprit.sb_first_project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tn.esprit.sb_first_project.entities.Inscription;
+import tn.esprit.sb_first_project.entities.Support;
+
+import java.util.List;
+
 @Repository
 public interface IInscriptionRepo extends JpaRepository<Inscription,Long> {
+
+    //afficher les numéros des semaines où un moniteur a donnée des cours selon un support donné.
+    //@Query("select i.numSemaine from Inscription i join Cours c, Moniteur m where c.support=: sup and c.m.NumMoniteur=: idM")
+    //List<Integer> getNumSemaineBySupportAndMoniteur(@Param("sup")Support support,@Param("idM") Long idM);
+    @Query("SELECT DISTINCT i.numSemaine FROM Inscription i " +
+            "JOIN i.cours c " +
+            "JOIN c.moniteur m " +
+            "WHERE m.numMoniteur = :moniteurId " +
+            "AND c.support = :support")
+    List<Integer> findWeekNumbersByMoniteurAndSupport(@Param("moniteurId") Long moniteurId, @Param("support") Support support);
 }
